6a284f88b32e1fe894e0eb7d43566ef8319714c1
aschambers <aschambers4291@gmail.com>
2018-01-29 02:07:45 -0800

    hello world

---
+++ JavaTutorials/app/src/main/java/com/aschambers/learnjava/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.aschambers.learnjava;
+
+/**
+ * Created by metabou on 1/28/18.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // tell the system class to print the line, part of java.lang
+        // contains an object called out
+        System.out.println("Hello World");
+    }
+}
