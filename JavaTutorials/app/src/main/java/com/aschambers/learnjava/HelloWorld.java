e43e87d971f57e79e2bccd1252bfc0e5b1fc9aec
aschambers <aschambers4291@gmail.com>
2018-01-29 02:12:53 -0800

    work on variables, strings, etc.

--- JavaTutorials/app/src/main/java/com/aschambers/learnjava/HelloWorld.java
+++ JavaTutorials/app/src/main/java/com/aschambers/learnjava/HelloWorld.java
@@ -9,5 +9,13 @@ public class HelloWorld {
         // tell the system class to print the line, part of java.lang
         // contains an object called out
         System.out.println("Hello World");
+
+        String Alan = "Alan Schambers";
+        System.out.println(Alan);
+
+        int hourly = 25;
+        int monthly = hourly * 4;
+        System.out.println(hourly);
+        System.out.println(monthly);
     }
 }
